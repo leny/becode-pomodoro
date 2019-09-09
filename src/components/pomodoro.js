ebfd94a3c740f118a857ef7c80fdd4707c4b8f33
leny <info@flatland.be>
2019-09-09 11:48:58 +0200

    Implement timer logic

--- src/components/pomodoro.js
+++ src/components/pomodoro.js
@@ -6,16 +6,40 @@
  * refactored at 09/09/2019
  */
 
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 import Display from "./display/display";
 import Tools from "./tools/tools";
 
-const DEFAULT_POMODORO_VALUE = 20;
+const DEFAULT_POMODORO_VALUE = 0.05;
 
 const Pomodoro = () => {
     const [running, setRunning] = useState(false);
     const [seconds, setSeconds] = useState(DEFAULT_POMODORO_VALUE * 60);
+    const [intervalId, setIntervalId] = useState(null);
+
+    useEffect(() => {
+        if (running) {
+            const id = setInterval(() => setSeconds(sec => sec - 1), 1000);
+
+            setIntervalId(id);
+        } else if (intervalId) {
+            clearInterval(intervalId);
+            setIntervalId(null);
+        }
+
+        return () => {
+            intervalId && clearInterval(intervalId);
+        };
+    }, [running]);
+
+    useEffect(() => {
+        if (seconds <= 0) {
+            setSeconds(0);
+            setRunning(false);
+            console.warn("Call modal!");
+        }
+    }, [seconds]);
 
     const handleMinus = () => {
         setSeconds(seconds < 60 ? 0 : seconds - 60);
