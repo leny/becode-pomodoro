2471cdd68937ee71ce4259eb3414bc1e3bffa1ff
leny <info@flatland.be>
2019-09-09 09:30:44 +0200

    Init application with tooling

---
+++ src/app.js
@@ -0,0 +1,12 @@
+/* leny/pomodoro
+ *
+ * /src/app.js - Main Entry point
+ *
+ * coded by leny@BeCode
+ * refactored at 09/09/2019
+ */
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<p>{"Hello, world!"}</p>, document.querySelector("#root"));
